c4d0d9992a3bcbe453268a643ef277a120024c1b
somcrizzy <nnakwechris@gmail.com>
2024-02-05 11:10:51 +0100

    done

---
+++ java.js
@@ -0,0 +1,4 @@
+function togglePasswordVisibility(inputId) {
+    var passwordInput = document.getElementById(inputId);
+    passwordInput.type = (passwordInput.type === 'password') ? 'text' : 'password';
+}
